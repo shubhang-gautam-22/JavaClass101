660be490c1f143ddd97ed6a87484a33988080b99
shubhang-gautam-22 <66652669+shubhang-gautam-22@users.noreply.github.com>
2020-06-17 17:38:18 -0500

    Add files via upload

---
+++ employees.java
@@ -0,0 +1,29 @@
+
+class employees {
+	private String name;
+	private String jobTitle;
+	private long phoneNumber;
+	
+	public String getName(){
+		return name;
+		
+	}
+	public void setName (String name){
+		this.name = name;
+		
+		
+	}
+    public String getJobTitle(){
+    	return jobTitle;
+    	
+    }
+    public void setJobTitle(){
+    	this.jobTitle = jobTitle;
+    }
+    public long getPhoneNumber(){
+    	return phoneNumber;
+    }
+    public void setPhoneNumber (){
+    	this.phoneNumber = phoneNumber;
+    }
+}
